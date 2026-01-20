d4923d8fc3884add566840ce1770c3cb63206943
r1v ubuntu <actuallyaun@gmail.com>
2026-01-20 16:02:52 +0500

    updated python file

--- Nuali.py
+++ Nuali.py
@@ -1,4 +1,7 @@
 import tweepy
+import time
+import random
+import logging
 
 apiKey="api_key"
 apiSecret="secret_key"
@@ -6,9 +9,96 @@ bearerToken=r"bearer_token"         # r=raw string, for special characters
 accessToken="access_token"
 accessTokenSecret="secret_access_token"
 
-client=tweepy.Client(bearerToken, apiKey, apiSecret, accessToken, accessTokenSecret)
+client=tweepy.Client(
+    bearerToken,
+    apiKey,
+    apiSecret,
+    accessToken,
+    accessTokenSecret,
+    wait_on_rate_limit=True
+)
 
-auth=tweepy.OAuth1UserHandler(apiKey, apiSecret, accessToken, accessTokenSecret)        # not essential but helps incase we want to use tweepy functions that need OAuth 1.0a
-api=tweepy.API(auth)
+auth=tweepy.OAuth1UserHandler(      # not essential but helps incase we want to use tweepy functions that need OAuth 1.0a
+    apiKey,
+    apiSecret,
+    accessToken,
+    accessTokenSecret
+)                               
+api = tweepy.API(auth)
 
-client.create_tweet(text="Hello world! I am Nuali.")
+logging.basicConfig(
+    filename="nuali.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+tweetInterval=60*60
+
+keywords=["python", "coding", "programming", "opensource"]
+
+jokes= [
+    "Why do programmers prefer dark mode? Because light attracts bugs.",         
+    "There are 10 types of people in the world: those who understand binary and those who do not.",
+    "I told my computer I needed a break, and it froze.",
+    "Why do Java developers wear glasses? Because they do not C#.",
+    "A SQL query walks into a bar, walks up to two tables and asks: Can I join you?",
+    "Debugging is like being the detective in a crime movie where you are also the murderer.",
+    "I changed my password to incorrect. Now whenever I forget it, the computer reminds me.",
+    "There is no place like 127.0.0.1.",
+    "Why did the programmer quit his job? Because he did not get arrays.",
+    "Programmers do not sleep, they just wait.",
+    "Hardware is the part of a computer you can kick.",
+    "A programmer is told: Go to the store and buy a loaf of bread. If they have eggs, buy a dozen. He comes back with twelve loaves of bread.",
+    "Real programmers count from zero."
+]
+
+replies=[
+    "HMMMMMMMMM, interesting.",
+    "Nah, I'd win.",
+    "Really?!.",
+    "LOOOOOOOOOOOOOOOOOOOOOOOOL",
+    "WOWOWOWOWWWW!"
+]
+
+def post_tweet():
+    tweet=random.choice(jokes)
+    client.create_tweet(text=tweet)
+    logging.info(f"Tweet posted: {tweet}")
+
+def interact_with_tweets():
+    query=" OR ".join(keywords) + " -is:retweet lang:en"
+    response=client.search_recent_tweets(
+        query=query,
+        max_results=5
+    )
+
+    if response.data is None:
+        return
+
+    for tweet in response.data:
+        try:
+            client.like(tweet.id)
+            try:
+                client.retweet(tweet.id)
+            except:
+                pass
+            reply=random.choice(replies)
+            client.create_tweet(
+                text=reply,
+                in_reply_to_tweet_id=tweet.id
+            )
+            logging.info(f"Interacted with tweet {tweet.id}")
+            time.sleep(20)
+        except Exception as e:
+            logging.error(str(e))
+
+def run():
+    while True:
+        try:
+            post_tweet()
+            interact_with_tweets()
+        except Exception as e:
+            logging.error(str(e))
+        time.sleep(tweetInterval)
+
+run()
